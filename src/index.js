497a2072c1fee1d90b910014e936b2bf7a0dc3c0
齐志新 <qzhxin@epoint.com.cn>
2022-11-21 14:39:55 +0800

    管理资源-加载images图像

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import './style.css'
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
@@ -7,6 +8,12 @@ function component() {
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello')
+
+      // 将图像添加到我们已经存在的 div 中。
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
   
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,11 @@ module.exports = {
         test: /\.css$/i, // 所有以 .css 结尾的文件，都将被提供给 style-loader 和 css-loade
         use: ['style-loader', 'css-loader'], // 应保证 loader 的先后顺序：'style-loader' 在前，而 'css-loader' 在后
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
+  
 };
