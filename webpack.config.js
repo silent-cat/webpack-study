65271093f910dd14ac4aee6ceec497db2119328f
齐志新 <qzhxin@epoint.com.cn>
2022-11-22 15:48:27 +0800

    管理输出-动态生成html并且动态添加bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,20 @@
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    index:'./src/index.js',
+    print:'./src/print.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i, // 所有以 .css 结尾的文件，都将被提供给 style-loader 和 css-loade
-        use: ['style-loader', 'css-loader'], // 应保证 loader 的先后顺序：'style-loader' 在前，而 'css-loader' 在后
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // 只生成用到的文件
   },
   
 };
