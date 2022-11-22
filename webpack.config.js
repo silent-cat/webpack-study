9d3a28729138caeb28ab9abe2283f398cecf4ccf
齐志新 <qzhxin@epoint.com.cn>
2022-11-22 16:13:30 +0800

    开发环境-自动编译代码

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
     index:'./src/index.js',
     print:'./src/print.js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist', // 告知 dev server，从什么位置查找文件
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: '管理输出',
@@ -15,6 +19,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true, // 只生成用到的文件
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   
 };
