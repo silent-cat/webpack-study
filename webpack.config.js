7b0f256212b750299fc5a9333e8cfc9de132acb3
齐志新 <qzhxin@epoint.com.cn>
2022-11-22 17:47:30 +0800

    代码分离-防止重复（SplitChunksPlugin）

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,23 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js',
-        another: './src/another-module.js',
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared', // 多个chunk共享模块
+          },
+          another: {
+            import: './src/another-module.js',
+            dependOn: 'shared',
+          },
+          shared: 'lodash',
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    optimization: {
+        splitChunks: {
+          chunks: 'all',
+        },
+      },
 };
